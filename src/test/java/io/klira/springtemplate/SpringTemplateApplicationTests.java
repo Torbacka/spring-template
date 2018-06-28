af1a15cdda503c4b1da50a5a90be52ee2b3909e3
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-06-28 15:18:20 +0200

    first commit

---
+++ src/test/java/io/klira/springtemplate/SpringTemplateApplicationTests.java
@@ -0,0 +1,17 @@
+package io.klira.springtemplate;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+class SpringTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
